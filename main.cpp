0527460f3bcb51d3813f405cc3ff8615340c26ac
SIMAOMARTA <up202406706@up.pt>
2026-05-11 14:52:30 +0100

    pequenas alterações

--- main.cpp
+++ main.cpp
@@ -1,27 +1,18 @@
 #include <iostream>
-/*
+#include "LiveRange.cpp"
+#include "Web.cpp"
+#include "Web.h"
+#include "LiveRange.h"
+
 #ifdef _WIN32
 #include <windows.h> // isto é só por causa dos acento e letras em windows dont worry
 #endif
 
-int main(int argc, char* argv[]) {
-
-#ifdef _WIN32
-    SetConsoleOutputCP(CP_UTF8);
-#endif
-
-    return 0;
-}
-*/
-
-
-//exemplo da figura 6
-
-#include <iostream>
-#include "Web.h"
-#include "LiveRange.h"
-
 int main() {
+
+    #ifdef _WIN32
+    SetConsoleOutputCP(CP_UTF8);
+    #endif
 
     Web sum(
         0,
